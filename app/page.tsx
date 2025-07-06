5e5e98c7a907f995d1c1546a2412ab04586e71e2
pa-khan <don@pa-khan.com>
2025-07-07 00:01:55 +0300

    redirect to products

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite'; // Используем mobx-react-lite для функц. компонентов
-import { BrandModel } from "@/core/models/brands/brand.model";
-import BaseLayout from "@/core/views/layouts/base.layout";
+import { useRouter } from "next/navigation";
 
 const HomePage = observer(() => {
-  const [brand] = useState(() => new BrandModel());
+  const router = useRouter();
   
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    brand.setV(e.target.value)
-  };
+  useEffect(() => {
+    // Редирект после монтирования компонента
+    router.push('/products');
+  }, []);
   
   return (
-    <BaseLayout>
-        <h1 className="text-2xl">Текущее значение: { brand.v }</h1>
-        
-        <input
-          type="text"
-          value={ brand.v }
-          onChange={ handleChange }
-          className="border border-gray-300 rounded px-2 py-1 mt-2"
-        />
-    </BaseLayout>
+    <>
+    </>
   );
 });
 
